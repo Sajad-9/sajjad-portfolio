838c236075efe27351471dba18385764ceddbad7
MohammadReza Alizadeh <alizadehmohammadreza1384@gmail.com>
2025-07-08 22:16:03 +0330

    set a basic url identifier for different routes based on the device that is running the application

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./styles/App.css";
 
 function App() {
+  const localHost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+  const BaseURL = localHost
+    ? "http://localhost:5000"
+    : "http://192.168.1.103:5000";
   return <div className="App"></div>;
 }
 
